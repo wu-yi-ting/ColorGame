c67f7980218050d550553621f6aa6eb97955ab5a
bellayaya <bella@17.media>
2020-05-07 18:14:38 +0800

    產生2x2矩陣

--- lib/game.dart
+++ lib/game.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class GamePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -17,12 +16,59 @@ class _GamePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: RaisedButton(
-        child: Text('返回首頁'),
-        onPressed: () {
-          Navigator.pop(context);
-        },
-      ),
+
+
+      child: _createGridView(),
+
+//      child: RaisedButton(
+//        child: Text('返回首頁'),
+//        onPressed: () {
+//          Navigator.pop(context);
+//        },
+//      ),
+    );
+  }
+}
+
+
+Widget _createGridView() {
+  return GridView.builder(
+    itemCount: 4,  //TODO:動態調整
+    shrinkWrap: true,
+    padding: EdgeInsets.all(10), // padding
+    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 2, // 每一行的个数
+      mainAxisSpacing: 10, // 间距
+      crossAxisSpacing: 10,
+    ),
+    itemBuilder: (context, index) {
+      return Container(
+        alignment: Alignment.center,
+        child: AspectRatio(
+          aspectRatio: 1, // 宽高比
+          child: _ColorRect()
+        ),
+      );
+    },
+  );
+}
+
+class _ColorRect extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        FlatButton(
+          color: Colors.orange, //顏色要random
+          child: AspectRatio(
+            aspectRatio: 1/1,
+          ),
+          onPressed: () {
+            //TODO: 按對 加1分 按錯沒反應
+          },
+        )
+      ],
     );
   }
 }
